5cbc32b3a8a52500dafc49166ec205efd8b07f8d
IyasuH <eyasuhailegbr@gmail.com>
2022-11-13 23:06:51 +0300

    setting up paramets on circularProgressBar class

--- lib/Widgets/circularProgressBars.dart
+++ lib/Widgets/circularProgressBars.dart
@@ -1,28 +1,43 @@
+// ignore: file_names
 import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
-class SalesCircualrProgressBar extends StatefulWidget {
-  const SalesCircualrProgressBar({super.key});
+// ignore: must_be_immutable
+class CircualrProgressBar extends StatefulWidget {
+  double value;
+  Color primaryColor;
+  Color secondaryColor;
+
+  CircualrProgressBar({
+    super.key,
+    // this value later gate from calcualting monthly sales change
+    required this.value,
+    required this.primaryColor,
+    required this.secondaryColor,
+  });
 
   @override
-  State<SalesCircualrProgressBar> createState() =>
-      _SalesCircualrProgressBarState();
+  State<CircualrProgressBar> createState() =>
+      // ignore: no_logic_in_create_state
+      _CircualrProgressBarState();
 }
 
-class _SalesCircualrProgressBarState extends State<SalesCircualrProgressBar> {
+class _CircualrProgressBarState extends State<CircualrProgressBar> {
   late Timer _timer;
   double salesProgressValue = 0;
+  _CircualrProgressBarState();
   // this value later gate from calcualting monthly sales change
-  double maxSalesValue = 37;
+  // double maxSalesValue = ;
+
   @override
   void initState() {
     super.initState();
     if (mounted) {
       _timer = Timer.periodic(const Duration(milliseconds: 30), (Timer timer) {
         setState(() {
-          if (salesProgressValue < maxSalesValue) {
+          if (salesProgressValue < widget().value) {
             salesProgressValue++;
           }
         });
@@ -43,9 +58,9 @@ class _SalesCircualrProgressBarState extends State<SalesCircualrProgressBar> {
             startAngle: 270,
             endAngle: 270,
             radiusFactor: 1,
-            axisLineStyle: const AxisLineStyle(
+            axisLineStyle: AxisLineStyle(
               thickness: 0.15,
-              color: Color.fromARGB(30, 0, 169, 181),
+              color: widget().secondaryColor,
               thicknessUnit: GaugeSizeUnit.factor,
             ),
             pointers: <GaugePointer>[
@@ -58,7 +73,7 @@ class _SalesCircualrProgressBarState extends State<SalesCircualrProgressBar> {
                 animationDuration: 75,
                 animationType: AnimationType.linear,
                 cornerStyle: CornerStyle.bothCurve,
-                color: Colors.cyan,
+                color: widget().primaryColor,
               ),
             ],
             annotations: <GaugeAnnotation>[
