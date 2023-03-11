ef70988b9b3e57c21f1255842ec5e48387c1103f
IyasuH <eyasuhailegbr@gmail.com>
2023-03-12 00:40:58 +0300

    year and month lists for dropdown buttons

---
+++ lib/models/yearMonthList.dart
@@ -0,0 +1,33 @@
+class CustomMonth {
+  final String numr;
+  final String accr;
+  CustomMonth({
+    required this.numr,
+    required this.accr,
+  });
+}
+
+List<String> myYear = [
+  '2022',
+  '2023',
+  '2024',
+  '2025',
+  '2026',
+  '2027',
+  '2028',
+  '2029'
+];
+List<CustomMonth> myDefinefMonth = [
+  CustomMonth(numr: '1', accr: 'Jan'),
+  CustomMonth(numr: '2', accr: 'Feb'),
+  CustomMonth(numr: '3', accr: 'Mar'),
+  CustomMonth(numr: '4', accr: 'Apr'),
+  CustomMonth(numr: '5', accr: 'May'),
+  CustomMonth(numr: '6', accr: 'Jun'),
+  CustomMonth(numr: '7', accr: 'Jul'),
+  CustomMonth(numr: '8', accr: 'Aug'),
+  CustomMonth(numr: '9', accr: 'Sep'),
+  CustomMonth(numr: '10', accr: 'Oct'),
+  CustomMonth(numr: '11', accr: 'Nov'),
+  CustomMonth(numr: '12', accr: 'Dec'),
+];
