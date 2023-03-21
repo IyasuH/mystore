96fe95d949bf2ca604d9c4e515e3d19f254b6e3c
IyasuH <eyasuhailegbr@gmail.com>
2023-03-21 12:01:07 +0300

    to load daily profit data

---
+++ lib/Widgets/dailyProfit.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DailyBarChart extends StatefulWidget {
+  const DailyBarChart({super.key});
+
+  @override
+  State<DailyBarChart> createState() => _DailyBarChartState();
+}
+
+class _DailyBarChartState extends State<DailyBarChart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
